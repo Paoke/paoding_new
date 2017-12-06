3d09ffbb48ffed4ae7b95d9ca1a3a04b3fb5c545
Ctoy <hxdtzy@126.com>
2017-12-06 15:01:42 +0800

    [12.6hx]

--- Application/Runtime/Cache/Mobile/e866b338bd4c8060ef03b59ff311d1dd.php
+++ Application/Runtime/Cache/Mobile/e866b338bd4c8060ef03b59ff311d1dd.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>需求</title>
-    <meta name="description" content="庖丁科技众包服务平台携手国内外先进技术科研院所、顶级技术专家，以实现科技成果市场化为核心，为企业提供快速精准的需求匹配服务，从而实现企业以及科技资源的有效对接，帮助企业实现产业技术升级，助力先进技术完成产业化发展。" />
+    <meta name="description" content="庖丁科技众包服务平台携手国内外先进技术科研院所、顶级技术专家，以实现科技成果市场化为核心，为企业提供快速精准的需求匹配服务，从而实现企业  以及科技资源的有效对接，帮助企业实现产业技术升级，助力先进技术完成产业化发展。" />
     <meta name="keywords" content="庖丁众包、智能技术、机械制造、健康医疗、材料科学、能源环保、生产流程优化"/>
     <meta name='viewport' content='user-scalable=no,width=750'>
     <link rel="stylesheet" href="<?php echo (MOBILE); ?>/css/common_new.css">
@@ -20,7 +20,7 @@
                     <div class="sildeBlock"></div>
                 </div>
                 <div class="swiper-slide" ng-repeat="list in lists">
-                    <li>{{list.cat_name}}</li>                
+                    <li ng-value="list.id">{{list.cat_name}}</li>                
                 </div>
             </div>
         </div>
@@ -29,12 +29,9 @@
 
 <div class="search clear">
     <p class="search_p fl">您当前选择的业务范围</p>
-    <select class="fl" name="" id="">
+    <select class="fl" name="" id="demand_type" ng-cloak>
         <option value="全部">全部</option>
-        <option value="科研类">科研类</option>
-        <option value="应用类">应用类</option>
-        <option value="专利类">专利类</option>
-        <option value="服务类">服务类</option>
+        <option ng-value="option.id" ng-repeat="option in options">{{option.tag_name}}</option>
     </select>
     <div class="inputDiv fr">
         <img src="<?php echo (MOBILE); ?>/images/icon-search-default.png" alt="">
@@ -128,7 +125,17 @@
             }, function errorCallback(response) {
             });
 
-        // 1.21 需求详情列表
+        // 1.21.获取需求业务范围接口
+        $http({
+            method:'GET',
+            url:'/api.php/ChannelIndex/index/action/dataList/channel/xq/type/4',
+        }).then(function successCallback(response) {
+            $scope.options = response.data.data;
+        }, function errorCallback(){
+
+        });
+
+        // 1.22.获取需求列表接口
         var data = {"page":"1","page_num":"10","order_field":"create_time","order_by":"DESC","category_id":"0","get_page":"true"};
         $http({
             method:'POST',
@@ -151,6 +158,63 @@
             });
     });
 
+        $(".demand_nav").on("click",".swiper-slide li",function(){
+            var index = $(this).parent(".swiper-slide").index();
+            var tranLeft = index*150;      
+            $(".sildeBlock").css("transform","translateX("+tranLeft+"px)");
+            // 1.23.按需求栏目和业务范围请求列表接口
+            var category_id =$(this).val();
+        
+            var tag_id = $("#demand_type").val();
+            var data = {"category_id":category_id,"tag_id":0};
+            // var info = $("").length;
+            var code = "";
+            $.post("/api.php/ChannelIndex/index/action/dataList/channel/xq/type/1",data,function(response){
+              console.log(response.data.length);
+              var info = response.data;
+                for(var i = 0;i<info.length;i++){
+                    code = code+
+                     '<div class="main">'+
+                         '<div class="section">'+
+                         '<p class="sec_title clear">'+
+                                '<span class="callForBids fl">【'+info[i].hzxs+'】</span>'+
+                                '<span class="materials fl">'+info[i].title+'</span>'+
+                         '</p>'+
+                         '<p class="sec_content">'+info[i].desc+'</p>'+
+                         '<div class="sec_data clear">'+
+                                '<div class="sec_data_left fl">'+
+                                   ' <div>'+
+                                        '<img src="<?php echo (MOBILE); ?>/images/icon-search-type.png" alt="">'+
+                                        '<span class="sec_data_span">'+info[i].cat_name+'</span>'+
+                                        '<img src="<?php echo (MOBILE); ?>/images/icon-search-time.png" alt="">'+
+                                        '<span class="sec_data_span">'+info[i].yfzq+'</span>'+
+                                        '<img src="<?php echo (MOBILE); ?>/images/icon-search-scan.png" alt="">'+
+                                        '<span class="sec_data_span">'+info[i].clicks+'</span>'+
+                                    '</div>'+
+                                    '<p class="sec_data_p">'+
+                                        '发布时间: <span class="sec_data_span">'+info[i].create_time+'</span>'+
+                                        '投入预算：<span>'+info[i].yfys+'</span>'+
+                                    '</p>'+
+                               ' </div>'+
+                                '<img class="fr" src="<?php echo (MOBILE); ?>/images/icon-research.png" alt="">'+
+                           ' </div>'+
+                            '<a href="/index.php/Mobile/Demand/detail">'+
+                               ' <div class="viewDetails clear">'+
+                                   ' <img class="fr" src="<?php echo (MOBILE); ?>/images/icon-right.png" alt="">'+
+                                   ' <span class="viewDetails_span fr">查看详情</span>'+
+                                '</div>'+
+                            '</a>'+
+                        '</div>'+
+                        '<div class="index_div"></div>'+
+                     '</div>';
+                }
+                $('.wrap').html(code);
+            })
+
+        })
+ 
+    
+    // 滚动加载
     $(window).scroll(function () {
             if ($(document).scrollTop() > ($(document).height() - $(window).height()) / 1.5) {
 
@@ -204,6 +268,14 @@
             }
         });
 
+    // 下拉请求  
+    $('select#demand_type').change(function() {
+        var data = $(this).val();
+         // $.get("/api.php/ChannelIndex/index/action/dataList/channel/xq/type/1",data,function(response){
+
+         // })
+    }); 
+
     // 轮播
     var mySwiper = new Swiper(".swiper-container",{
         freeMode : true,
@@ -212,11 +284,7 @@
         observeParents:true//修改swiper的父元素时，自动初始化swiper 
     });
 
-    $(".demand_nav").on("click",".swiper-slide li",function(){
-        var index = $(this).parent(".swiper-slide").index();
-        var tranLeft = index*150;      
-        $(".sildeBlock").css("transform","translateX("+tranLeft+"px)");
-    });
+
 
     $(".inputDiv").on("click",function(){
         $(".search").hide();
