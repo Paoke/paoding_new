03e86a253f7e98f72274a1a937c493d6254faa1a
Ctoy <hxdtzy@126.com>
2018-01-18 11:36:14 +0800

    [1.18压缩]

--- Public/mobile/js/return.js
+++ Public/mobile/js/return.js
@@ -1,2 +1,1 @@
-var returnNode = '<div class="sec_return"><a onclick="javascript:history.go(-1);"><img class="return_icon" src="/Public/mobile/images/icon-common-return.png" alt=""></a></div>';
-$('body').append(returnNode);
+var returnNode='<div class="sec_return"><a onclick="javascript:history.go(-1);"><img class="return_icon" src="/Public/mobile/images/icon-common-return.png" alt=""></a></div>';$("body").append(returnNode);
