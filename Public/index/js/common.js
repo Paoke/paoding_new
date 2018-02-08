f352b3194409bcb3aa23be4cd5bcc82690f20984
Ctoy <hxdtzy@126.com>
2018-02-08 17:57:55 +0800

    [2.8hx]

--- Public/index/js/common.js
+++ Public/index/js/common.js
@@ -129,6 +129,24 @@ $("body").on("click",".sug_inp3",function(){
     time($('.sug_inp3'));
 });
 
+// 微信分享
+  window._bd_share_config = {
+    "common" : {
+     "bdSnsKey" : {},
+     "bdText" : "",
+     "bdMini" : "2",
+     "bdUrl" : window.location.href,
+     "bdMiniList" : false,
+     "bdPic" : "",
+     "bdStyle" : "0", 
+    },
+    "share" : { "bdSize" : "32"}
+  };
+  with (document)
+    0[(getElementsByTagName('head')[0] || body)
+      .appendChild(createElement('script')).src = 'http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='
+      + ~(-new Date() / 36e5)];
+
 //退出登录
 $("#logout").on("click",function() {
     $.ajax({
